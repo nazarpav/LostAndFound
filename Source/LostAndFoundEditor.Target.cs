403e1ad1e54faf1dd00d91b3b92be75e538117e5
NazarPav <hesab.k18@gmail.com>
2021-01-30 01:35:20 +0200

    init

---
+++ Source/LostAndFoundEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class LostAndFoundEditorTarget : TargetRules
+{
+	public LostAndFoundEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("LostAndFound");
+	}
+}
